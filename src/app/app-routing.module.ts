09d023ff3b8ffa4d467c26955ef10673ebd54d2d
sebas <sebastian.1701615348@ucaldas.edu.co>
2019-07-20 19:59:22 -0500

    version 1.0.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneralModule } from './general/general.module';
+import { HomeComponent } from './general/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    GeneralModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/general/general.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [HomeComponent, PageNotFoundComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class GeneralModule { }
